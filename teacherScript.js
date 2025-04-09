95a83d24a8f6a407f5d311187ad6ce602a17e38a
KevinDASC22 <109813046+KevinDASC22@users.noreply.github.com>
2025-04-09 17:00:58 +0000

    finished

--- teacherScript.js
+++ teacherScript.js
@@ -1,2 +1,13 @@
 // Add an event listener to the form that upon submits creates a new teacher
 // When the teacher is created redirect to the teachers page
+const createForm= document.querySelector("form")
+
+createForm.addEventListener("submit", async(e)=>{
+    e.preventDefault()
+
+const teacherin = new FormData("form")
+const reqBody = Object.fromEntries(teacherin)
+const response = await fetch("/add/teacher" , {
+
+})
+})
